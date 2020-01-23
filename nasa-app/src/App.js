23b7538ae19a2209ca8bb3f7f3dded64edf73055
BrandyBecker <brandybeckerfsw@gmail.com>
2020-01-23 17:59:03 -0500

    Added functioning DatePicker

--- nasa-app/src/App.js
+++ nasa-app/src/App.js
@@ -3,14 +3,25 @@ import axios from 'axios'
 import './App.css';
 import Structure from './components/Structure'
 import Footer from './components/Footer'
+import moment from 'moment'
 
-function App() {
+import "react-datepicker/dist/react-datepicker.css";
+
+
+function App (props) {
 
   const [data, setData] = useState([])
+  const [startDate, setStartDate] = useState(Date.now())
+  
+  const handleChange = date => {
+   setStartDate(date);
+  };
 
   useEffect(()=>{
+    const date = moment(startDate).format("YYYY-MM-DD");
+ 
     axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=WSzgyshEH2w05Jecoz4QDuuLylIM8Z3EqZSZ8Xwr')
+      .get(`https://api.nasa.gov/planetary/apod?api_key=WSzgyshEH2w05Jecoz4QDuuLylIM8Z3EqZSZ8Xwr&date=${date}`)
       .then(res => {
         console.log("RESPONSE DATA:", res.data)
         setData(res.data)
@@ -18,13 +29,14 @@ function App() {
       .catch(err => {
         console.log("ERROR Fetching API:", err)
       })
-  },[])
+
+      console.log("StartDateProps", startDate);
+  },[startDate])
  
 
   return (
     <div className="App">
-
-        <Structure data={data}/>
+        <Structure selected={startDate} handleChange={handleChange} data={data}/>
         <Footer/>
     </div>
   );
--- nasa-app/src/components/Structure.js
+++ nasa-app/src/components/Structure.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import DatePicker from "react-datepicker";
 
 const Structure = props => {
 
     return (
         <div className="card-structure" style={{backgroundImage: `url(${props.data.hdurl})` }}>
+
             <h3 className="card-title">✨ {props.data.title} ✨</h3>
-            <p className="card-date">{props.data.date}</p>
+
+                <div className="card-date">
+                    <DatePicker selected={props.selected} onChange={props.handleChange} />
+                </div>
 
             <p className="card-bio">{props.data.explanation}</p>
             
